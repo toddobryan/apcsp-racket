c597c0030426b7542ec8a4acdf29b3e3cffecc04
Todd O'Bryan <toddobryan@gmail.com>
2025-11-16 23:43:52 -0500

    Initial commit

---
+++ apcsp-racket/info.rkt
@@ -0,0 +1,17 @@
+#lang info
+(define collection 'multi)
+
+(define deps '("apcsp-racket-lib"
+               "apcsp-racket-doc"
+               "apcsp-racket-tool"))
+
+(define implies '("apcsp-racket-lib"
+                  "apcsp-racket-doc"
+                  "apcsp-racket-tool"))
+
+
+(define pkg-desc "Racket for AP Computer Science Principles")
+
+(define pkg-authors '(toddobryan))
+
+(define license '(Apache-2.0 OR MIT))
