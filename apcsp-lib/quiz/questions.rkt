7558c0ce813512a8a558d557fa20cd18db0d41d0
Todd O'Bryan <toddobryan@gmail.com>
2026-09-03 13:00:57 -0400

    questions?

--- apcsp-lib/quiz/questions.rkt
+++ apcsp-lib/quiz/questions.rkt
@@ -1,1 +1,20 @@
 #lang racket
+(define (signature-print sig port mode)
+  (let* ([inputs (string-join (map symbol->string (signature-inputs sig))
+                             #:before-first "(" #:after-last " -> ")]
+         [str (string-join "(: " 
+    
+    (write "(: " port)
+    (write (signature-name sig) port)
+    (write " " port)
+    (write (string-join (map symbol->string (signature-inputs sig)) #:before-first "(" #:after-last " -> ") port) 
+    (write (signature-output sig) port)
+    (write "))" port))
+
+
+(struct signature (name inputs output)
+  #:transparent
+  #:methods gen:custom-write
+  [(define write-proc signature-print)])
+
+(define func (signature 'func '(Number Boolean) 'Number))
